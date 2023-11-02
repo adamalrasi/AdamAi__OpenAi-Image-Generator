7c034666d7150430b5f9e256faed7924339a9eaf
Adam <adam.alrasi@outlook.com>
2023-11-02 03:58:13 +0000

    fix for logo

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -2,5 +2,6 @@ import download from "./download.png";
 import logo from "./logo.svg";
 import preview from "./preview.png";
 import pixel from "./pixel.png";
+import newLogo from "./adam-logo.jpg";
 
-export { download, logo, preview, pixel };
+export { download, logo, preview, pixel, newLogo };
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import { Loader, Card, FormField } from "../components";
 import { useNavigate } from "react-router-dom";
 
-import { preview, pixel } from "../assets";
+import { preview, pixel, newLogo } from "../assets";
 // import { pixel } from "../assets";
 import { getRandomPrompt } from "../utils";
 
